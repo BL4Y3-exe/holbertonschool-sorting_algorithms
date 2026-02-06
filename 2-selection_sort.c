b39f31068c0ca2482fb00b94d65d82178fc59180
Ilknur <ilknurkazimov308@gmail.com>
2026-02-06 17:20:28 +0400

    Add 2-selection_sort.c code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int temp;
+	size_t i, j, min;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min] > array[j])
-            {
-                min = j;
-            }
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+			}
+		}
 
-        if (min != i)
-        {
-            temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            print_array(array, size);
-        }
-    }
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
